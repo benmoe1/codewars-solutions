8dc2a37a1623263c866a417417ae83c5d658a143
benmoe <b.moelling@googlemail.com>
2017-12-08 19:44:56 +0100

    added kata

---
+++ 8/firstNonConsecutive.js
@@ -0,0 +1,12 @@
+function firstNonConsecutive(arr) {
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] !== (arr[i] + 1)) {
+      return arr[i + 1];
+    }
+  }
+
+  return null
+}
+console.log(firstNonConsecutive([1,2,3,5])) // 5
+console.log(firstNonConsecutive([1,2,3,4,5])) // null
