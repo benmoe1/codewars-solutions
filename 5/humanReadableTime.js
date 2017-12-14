69e26f6ee13c33e3a181e0dccaf4433493cdf6e9
benmoe <b.moelling@googlemail.com>
2017-12-14 21:49:36 +0100

    added kata

---
+++ 5/humanReadableTime.js
@@ -0,0 +1,19 @@
+function humanReadable(seconds) {
+  
+    function humanReadableFormat(input) {
+      if (input < 10) {
+        return "0" + input;
+      } else {
+        return input;
+      }
+    }
+  
+    const inputSeconds = humanReadableFormat(seconds % 60);
+    const inputMinutes = humanReadableFormat(Math.floor(seconds / 60) % 60);
+    const inputHours = humanReadableFormat(Math.floor((seconds / 60) / 60));
+  
+    return inputHours + ':' + inputMinutes + ':' + inputSeconds;
+  }
+  console.log(humanReadable(60)) // 00:01:00
+  console.log(humanReadable(86399)) // 23:59:59
+  console.log(humanReadable(359999)) // 99:59:59
