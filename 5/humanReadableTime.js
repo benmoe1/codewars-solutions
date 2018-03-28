853f908823b686ed2d8d11382e18bbd7e836076d
benmoe <b.moelling@googlemail.com>
2018-03-28 11:18:53 +0200

    simplified humanReadable
    removed unneccessary else case and changed string concatenation to template strings

--- 5/humanReadableTime.js
+++ 5/humanReadableTime.js
@@ -1,19 +1,17 @@
 function humanReadable(seconds) {
-  
-    function humanReadableFormat(input) {
-      if (input < 10) {
-        return "0" + input;
-      } else {
-        return input;
-      }
+
+  function humanReadableFormat(input) {
+    if (input < 10) {
+      input = `0${input}`;
     }
-  
-    const inputSeconds = humanReadableFormat(seconds % 60);
-    const inputMinutes = humanReadableFormat(Math.floor(seconds / 60) % 60);
-    const inputHours = humanReadableFormat(Math.floor((seconds / 60) / 60));
-  
-    return inputHours + ':' + inputMinutes + ':' + inputSeconds;
+    return input;
   }
-  console.log(humanReadable(60)) // 00:01:00
-  console.log(humanReadable(86399)) // 23:59:59
-  console.log(humanReadable(359999)) // 99:59:59
+
+  const inputSeconds = humanReadableFormat(seconds % 60);
+  const inputMinutes = humanReadableFormat(Math.floor(seconds / 60) % 60);
+  const inputHours = humanReadableFormat(Math.floor((seconds / 60) / 60));
+  return `${inputHours}:${inputMinutes}:${inputSeconds}`;
+}
+console.log(humanReadable(60)) // 00:01:00
+console.log(humanReadable(86399)) // 23:59:59
+console.log(humanReadable(359999)) // 99:59:59
